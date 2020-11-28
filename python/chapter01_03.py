612d0a934d8080980c241824ab781274c3b88ed2
Jack Lee <krdboxer@gmail.com>
2020-11-28 15:59:25 +0900

    chapter01 - 클래스 메소드, 인스턴스 메소드

--- python/chapter01_03.py
+++ python/chapter01_03.py
@@ -53,6 +53,18 @@ class Student(object):
         cls.tuition_per = per
         print('Succed! tuition increased')
 
+    # Class Method
+    @classmethod
+    def student_const(cls, id, first_name, last_name, email, grade, tuition, gpa):
+        return cls(id, first_name, last_name, email, grade, tuition * cls.tuition_per, gpa)
+
+    # Static Method
+    @staticmethod
+    def is_scholarship_st(inst):
+        if inst._gpa > 4.3:
+            return '{} is a scholarship recipient.'.format(inst._last_name)
+        return 'Sorry. Not a scholarship recipient.'
+
 
 student_1 = Student(1, 'Kim', 'Sarang', 'student1@naver.com', '1', 400, 3.5)
 student_2 = Student(2, 'Lee', 'Myungho', 'student2@daum.net', '2', 500, 4.3)
@@ -82,11 +94,45 @@ Student.raise_fee(1.5)
 # 학비 정보 (인상후)
 print(student_1.get_fee_calc())
 print(student_2.get_fee_calc())
+print()
 
 
+# 클래스 메소드 인스턴스 생성
+student_3 = Student.student_const(3, 'Park', 'Minji', 'Student3@gmail.com', '3', 550, 4.5)
+student_4 = Student.student_const(4, 'Cho', 'Sunghan', 'Student4@gmail.com', '4', 600, 4.1)
 
+# 전체 정보
+print(student_3.detail_info())
+print(student_4.detail_info())
+print()
 
+# 학생 학비 변경 확인
+print(student_3._tuition)
+print(student_4._tuition)
+print()
 
+# 장학금 혜택 여부(스테틱 메소드 미사용)
+def is_scholarship(inst):
+    if inst._gpa > 4.3:
+        return '{} is a scholarship recipient.'.format(inst._last_name)
+    return 'Sorry. Not a scholarship recipient.'
+
+print(is_scholarship(student_1))
+print(is_scholarship(student_2))
+print(is_scholarship(student_3))
+print(is_scholarship(student_4))
+
+print()
+
+# 장학금 혜택 여부(스테틱 메소드 사용)
+print(Student.is_scholarship_st(student_1))
+print(Student.is_scholarship_st(student_2))
+print(Student.is_scholarship_st(student_3))
+print(Student.is_scholarship_st(student_4))
+
+print()
+
+print(student_1.is_scholarship_st(student_1))
 
 
 
