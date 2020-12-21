22cafa588b65090473cf91ce52abf89bac39cf88
Jack Lee <krdboxer@gmail.com>
2020-12-21 22:12:53 +0900

    Django 기초 - 20/12/21

--- fc_community/fcuser/admin.py
+++ fc_community/fcuser/admin.py
@@ -4,6 +4,7 @@ from .models import Fcuser
 # Register your models here.
 
 class FcuserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('username', 'password')
+
 
 admin.site.register(Fcuser, FcuserAdmin)
